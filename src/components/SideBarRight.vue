dc18a83e3b9f547ae5a50de08d9ee3595985563b
Kibet Ronoh <kibethillaire@gmail.com>
2021-10-20 18:37:47 +0300

    Update

---
+++ src/components/SideBarRight.vue
@@ -0,0 +1,167 @@
+<template>
+  <v-navigation-drawer app color="grey lighten-3" right width="270">
+    <v-toolbar color="grey lighten-3" flat>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-badge bordered overlap color="red" dot>
+          <v-icon>far fa-bell</v-icon>
+        </v-badge>
+      </v-btn>
+      <v-badge bordered bottom color="teal" dot offset-x="10" offset-y="10">
+        <v-avatar size="40" rounded>
+          <v-img src="0.png" max-height="100" max-width="100"></v-img>
+        </v-avatar>
+      </v-badge>
+      <v-btn icon>
+        <v-icon>fas fa-ellipsis-v</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-toolbar class="pl-0 mt-5" flat color="grey lighten-8">
+      <h3>My Orders</h3>
+      <v-spacer></v-spacer>
+      <h6 class="teal--text">Edit</h6>
+    </v-toolbar>
+    <v-app-bar flat color="rgba(0,0,0,0)" class="mt-n5">
+      <v-btn fab x-small color="teal lighten-4" elevation="0">
+        <v-icon color="teal">fas fa-clock</v-icon>
+      </v-btn>
+      <h5 class="ml-2 grey--text text--darken-2">17:30 PM</h5>
+    </v-app-bar>
+
+    <v-app-bar flat color="rgba(0,0,0,0)" class="mt-n5">
+      <v-btn fab x-small color="teal lighten-4" elevation="0">
+        <v-icon color="teal">fas fa-map-marker-alt</v-icon>
+      </v-btn>
+      <h5 class="ml-2 grey--text text--darken-2">29 Funzi Road, Nairobi</h5>
+    </v-app-bar>
+
+    <v-app-bar flat color="rgba(0,0,0,0)" class="mt-n5">
+      <v-btn fab x-small color="teal lighten-4" elevation="0">
+        <v-icon color="teal">fas fa-comment-dots</v-icon>
+      </v-btn>
+      <h5 class="ml-2 grey--text text--darken-2">Any Question?</h5>
+    </v-app-bar>
+    
+    <v-divider></v-divider>
+
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="3">
+          <v-img src="0.png" max-height="50" max-width="50" contain class="mt-1 mr-1"></v-img>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <h6 class="ml-n6 grey--text text--darken-2">Chicken Salad</h6>
+          <span class="grey--text text--lighten-1">987grams</span>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-chip class="ml-n5" color="teal" outlined text-color="black" small>
+            <v-icon left x-small>
+              fas fa-plus
+            </v-icon>
+            <v-divider vertical inset class="mr-1"></v-divider>
+            1
+            <v-divider vertical inset class="mr-1"></v-divider>
+            <v-icon right x-small>
+              fas fa-minus
+            </v-icon>
+          </v-chip>
+          <strong class="ml-1 mr-1">$5.08</strong>
+          <v-icon>
+            fas fa-times
+          </v-icon>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="3">
+          <v-img src="2.png" max-height="50" max-width="50" contain class="mt-1 mr-1"></v-img>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <h6 class="ml-n6 grey--text text--darken-2">Miwa Chai</h6>
+          <span class="grey--text text--lighten-1">2Kgs</span>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-chip class="ml-n5" color="teal" outlined text-color="black" small>
+            <v-icon left x-small>
+              fas fa-plus
+            </v-icon>
+            <v-divider vertical inset class="mr-1"></v-divider>
+            1
+            <v-divider vertical inset class="mr-1"></v-divider>
+            <v-icon right x-small>
+              fas fa-minus
+            </v-icon>
+          </v-chip>
+          <strong class="ml-1 mr-1">$12.00</strong>
+          <v-icon x-small>
+            fas fa-times
+          </v-icon>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="3">
+          <v-img src="0.png" max-height="50" max-width="50" contain class="mt-1 mr-1"></v-img>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <h6 class="ml-n6 grey--text text--darken-2">Chicken Salad</h6>
+          <span class="grey--text text--lighten-1">896Grms</span>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-chip class="ml-n5" color="teal" outlined text-color="black" small>
+            <v-icon left x-small>
+              fas fa-plus
+            </v-icon>
+            <v-divider vertical inset class="mr-1"></v-divider>
+            1
+            <v-divider vertical inset class="mr-1"></v-divider>
+            <v-icon right x-small>
+              fas fa-minus
+            </v-icon>
+          </v-chip>
+          <strong class="ml-1 mr-1">$4.09</strong>
+          <v-icon x-small>
+            fas fa-times
+          </v-icon>
+        </v-col>
+      </v-row>      
+    </v-container>
+    <div class="text-center">
+      <v-btn class="px-12" outlined color="teal">Drag & Drop</v-btn>
+    </div>
+    <v-toolbar class="pl-0" flat color="grey lighten-3">
+      <h3>Total</h3>
+      <v-spacer></v-spacer>
+      <h3>$15.98</h3>
+    </v-toolbar>
+    <div class="text-center">
+      <v-btn class="px-15" rounded color="teal" dark>
+        Checkout
+      </v-btn>
+    </div>
+    <div class="text-center">
+      <v-card elevation="2" class="mt-2 mx-2 pa-3">
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="2">
+              <v-icon color="blue" class=ml-n2>fab fa-cc-visa</v-icon>
+            </v-col>
+            <v-col cols="12" sm="2"><span class="caption text-9xl ml-n3">****</span></v-col>
+            <v-col cols="12" sm="2"><span class="caption text-9xl ml-n5">****</span></v-col>
+            <v-col cols="12" sm="2"><span class="caption text-9xl ml-n5">****</span></v-col>
+            <v-col cols="12" sm="2"><span class="caption text-9xl ml-n5">5132</span></v-col>
+            <v-col cols="12" sm="2"><v-icon small>fas fa-edit</v-icon></v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </div>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,103 @@
+<template>
+<v-app id="inspire">
+  <SideBarLeft />
+  <SideBarRight />
+  <v-container>
+    <v-toolbar flat>
+      <v-row>
+        <v-col cols="12" sm="10">
+          <v-text-field label="Search" filled rounded dense solo flat background-color="grey lighten-3"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <v-btn>
+            <v-icon>fas fa-filter</v-icon>Filter
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-toolbar>
+    <v-row class="mt-n9">
+      <v-col cols="12" sm="6">
+        <v-toolbar class="" flat>
+          <h5>My Filters</h5>
+          <v-icon class="ml-1" small>fas fa-car-side</v-icon>
+          <h5 class="ml-1">First Meal</h5>
+          <v-chip class="ma-2" color="teal" text-color="white" dense>
+            <v-avatar left>
+              <v-icon>fas fa-running</v-icon>
+            </v-avatar>
+            After Training
+          </v-chip>
+          <v-spacer></v-spacer>
+          <h6 class="teal-text">ADD</h6>
+        </v-toolbar>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-toolbar class="" flat>
+          <h5>popular Filters</h5>
+          <v-icon class="ml-1" small>fas fa-child</v-icon>
+          <h5 class="ml-1">Slim Waist</h5>
+          <v-spacer></v-spacer>
+          <v-icon class="ml-1">fas fa-birthday-cake</v-icon>
+          <h5 class="ml-1">Diet</h5>
+        </v-toolbar>
+      </v-col>
+    </v-row>
+    <v-toolbar class="pl-0 mt-n7" flat>
+      <h4>234</h4>
+      <h4 class="grey--text text--lighten-1 ml-2">Meals Found</h4>
+      <v-spacer></v-spacer>
+      <h4 class="grey--text text--lighten-1">Sorted by</h4>
+      <h4 class="ml-1">Recommended</h4>
+      <v-icon class="ml-1">fas fa-chevron-down</v-icon>
+    </v-toolbar>
+    <v-row class="mt-n6">
+
+
+      
+      <v-col cols="12" sm="4">
+        <v-hover v-dlot="{hover}" open-delay="200">
+          <v-card color="red lighten-5" :elevation="hover ? 16: 2">
+            <v-app-bar flat color="rgba(0,0,0,0)">
+              <v-chip class="ma-2" color="black" text-color="white" dense>
+                <v-avatar left>
+                  <v-icon color="yellow">fas fa-star</v-icon>
+                </v-avatar>
+                4.2
+              </v-chip>
+              <v-spacer></v-spacer>
+              <v-btn fab x-small color="white"><v-icon>far fa-heart</v-icon></v-btn>
+            </v-app-bar>
+            <div class="d-flex flex-column justify-space-between align-center">
+              <v-img src="0.png" max-height="100" max-width="100"></v-img>
+            </div>
+            <v-app-bar flat color="rgba(0,0,0,0)">
+              <h5 class="ml-1 grey--text text-lighten-3">Chicken Salad</h5>
+              <v-chip class="ma-2" color="grey lighten-3" text-color="red" dense>
+                250g
+              </v-chip>
+            </v-app-bar>
+            <h5 class="ml-5 mt-n5">$4.98</h5>
+          </v-card>
+        </v-hover>
+      </v-col>
+      
+
+
+    </v-row>
+  </v-container>
+</v-app>
+</template>
+
+<script>
+  import SideBarLeft from '../components/SideBarLeft'
+  import SideBarRight from '../components/SideBarRight'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      SideBarLeft,
+      SideBarRight
+    },
+  }
+</script>
