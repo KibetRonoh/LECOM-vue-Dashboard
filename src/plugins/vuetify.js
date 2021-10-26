8dbd6b2424c1c10bdde0781e303357d4d3af989a
Kibet Ronoh <kibethillaire@gmail.com>
2021-10-26 19:54:57 +0300

    On branch master

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,6 @@ import Vuetify from 'vuetify/lib/framework';
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify);
 
